7bcc674508c5cd39536004c156ef5f617ae34b11
aromatic-toast <34222068+aromatic-toast@users.noreply.github.com>
2019-01-09 22:58:21 -0800

    Add files via upload

---
+++ diff21.R
@@ -0,0 +1,10 @@
+#given an input value, n, return the absolute difference between n and 21. 
+#return double the absolute differene if n is > 21
+
+differ <- function(n){
+      if(n <= 21){
+         return(abs(21-x))   
+      } else if(x > 21){
+            return(2*abs(21-x))
+      }
+}
